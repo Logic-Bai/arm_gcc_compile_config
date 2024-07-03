52ea59dd53d45b6715292b3671d8a5ba4bcc1fa6
logic.bai <logicbai@foxmail.com>
2024-07-03 09:44:51 +0800

    1. Added the function of modifying the registry, and supported deleting the old registry when installing a new config

---
+++ arm_gcc_compile_config_required_tools/add_make.ps1
@@ -0,0 +1,20 @@
+# add_make.ps1
+
+$oldPath = [Environment]::GetEnvironmentVariable('Path', [System.EnvironmentVariableTarget]::Machine)
+
+$currentPath = (Get-Location).ToString()
+
+$pathEntries = $oldPath -split ';'
+$newPathEntries = @()
+$excludePath1 = '\arm_gcc_compile_config_required_tools'
+$excludePath2 = '\arm-gnu-toolchain-13.2.Rel1-mingw-w64-i686-arm-none-eabi\bin'
+foreach ($entry in $pathEntries) {
+    if ($entry -notlike "*$excludePath1*" -and $entry -notlike "*$excludePath2*") {
+        $newPathEntries += $entry
+    }
+}
+$newPath = $newPathEntries -join ';'
+$newPath = $newPath + ';' + $currentPath + $excludePath1 + ';' + $currentPath + $excludePath2
+[Environment]::SetEnvironmentVariable('Path', $newPath, [System.EnvironmentVariableTarget]::Machine)
+Write-Output "The new PATH is: $newPath"
+
--- install.bat
+++ install.bat
@@ -1,4 +1,6 @@
-if not exist arm-gnu-toolchain-13.2.rel1-mingw-w64-i686-arm-none-eabi.zip\arm-gnu-toolchain-13.2.Rel1-mingw-w64-i686-arm-none-eabi\bin\arm-none-eabi-gcc.exe (
+cd /d %~dp0
+
+if not exist arm-gnu-toolchain-13.2.Rel1-mingw-w64-i686-arm-none-eabi\bin\arm-none-eabi-gcc.exe (
    if not exist arm-gnu-toolchain-13.2.rel1-mingw-w64-i686-arm-none-eabi.zip (
       powershell -Command "Invoke-WebRequest https://armkeil.blob.core.windows.net/developer/Files/downloads/gnu/13.2.rel1/binrel/arm-gnu-toolchain-13.2.rel1-mingw-w64-i686-arm-none-eabi.zip -OutFile arm-gnu-toolchain-13.2.rel1-mingw-w64-i686-arm-none-eabi.zip"
       if exist arm-gnu-toolchain-13.2.rel1-mingw-w64-i686-arm-none-eabi.zip (
@@ -9,4 +11,12 @@ if not exist arm-gnu-toolchain-13.2.rel1-mingw-w64-i686-arm-none-eabi.zip\arm-gn
    )
 )
 
-setx arm_gcc_compile_config "%cd%" /m
+set arm_gcc_compile_config_path="%cd%"
+
+echo %arm_gcc_compile_config_path%
+
+setx /m arm_gcc_compile_config %arm_gcc_compile_config_path%
+
+powershell -NoProfile -ExecutionPolicy Bypass -File "arm_gcc_compile_config_required_tools\add_make.ps1"
+
+pause
